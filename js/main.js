13bd4a1dec364088d86b591a8f3be1540a156ea2
DaviGel <geloso.davide@gmail.com>
2023-11-08 20:03:06 +0100

    fix: migliorato codice

--- js/main.js
+++ js/main.js
@@ -17,18 +17,18 @@ elementUl.classList.add(
 for (let i = 1; i <= 100; i++) {
   const elementLi = document.createElement('li');
   elementLi.classList.add('element-align');
-  if (i % 3 === 0 && i % 5 !== 0) {
-    console.log(fizz);
-    elementLi.append(fizz);
-    elementLi.classList.add('bg-fizz');
-  } else if (i % 5 === 0 && i % 3 !== 0) {
-    console.log(buzz);
-    elementLi.append(buzz);
-    elementLi.classList.add('bg-buzz');
-  } else if (i % 3 === 0 && i % 5 === 0) {
+  if (i % 3 === 0 && i % 5 === 0) {
     console.log(fizzbuzz);
     elementLi.append(fizzbuzz);
     elementLi.classList.add('bg-fizzbuzz');
+  } else if (i % 5 === 0) {
+    console.log(buzz);
+    elementLi.append(buzz);
+    elementLi.classList.add('bg-buzz');
+  } else if (i % 3 === 0) {
+    console.log(fizz);
+    elementLi.append(fizz);
+    elementLi.classList.add('bg-fizz');
   } else {
     console.log(i);
     elementLi.append(i);
