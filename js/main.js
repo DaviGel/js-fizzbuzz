95cbec35d8007228d6b1af9fe82319600d615b26
DaviGel <geloso.davide@gmail.com>
2023-11-08 17:29:30 +0100

    feat: esercizio completato

--- js/main.js
+++ js/main.js
@@ -1,1 +1,41 @@
 'use strict';
+
+const fizz = 'Fizz';
+const buzz = 'Buzz';
+const fizzbuzz = 'FizzBuzz';
+
+const elementUl = document.getElementById('list');
+
+elementUl.classList.add(
+  'display-flex',
+  'flex-wrap',
+  'gap-20',
+  'list-style-none',
+  'dimentions'
+);
+
+for (let i = 1; i <= 100; i++) {
+  const elementLi = document.createElement('li');
+  elementLi.classList.add('element-display-flex');
+  if (i % 3 === 0 && i % 5 !== 0) {
+    console.log(fizz);
+    elementLi.append(fizz);
+    elementUl.append(elementLi);
+    elementLi.classList.add('bg-fizz');
+  } else if (i % 5 === 0 && i % 3 !== 0) {
+    console.log(buzz);
+    elementLi.append(buzz);
+    elementUl.append(elementLi);
+    elementLi.classList.add('bg-buzz');
+  } else if (i % 3 === 0 && i % 5 === 0) {
+    console.log(fizzbuzz);
+    elementLi.append(fizzbuzz);
+    elementUl.append(elementLi);
+    elementLi.classList.add('bg-fizzbuzz');
+  } else {
+    console.log(i);
+    elementLi.append(i);
+    elementUl.append(elementLi);
+    elementLi.classList.add('bg-element');
+  }
+}
